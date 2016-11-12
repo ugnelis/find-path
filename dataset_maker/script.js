795702d172befa97f49439afe3cd8119c4387416
Ugnelis <ugnius.malukas@gmail.com>
2016-11-12 02:02:30 +0200

    Added polygon moving functionality. Not finished yet.

--- dataset_maker/script.js
+++ dataset_maker/script.js
@@ -10,7 +10,7 @@
     var canvas = this.__canvas = new fabric.Canvas('canvas', {selection: false});
     //fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
 
-    function makeCircle(left, top, line1, line2) {
+    /*function makeCircle(left, top, line1, line2) {
         var c = new fabric.Circle({
             left: left,
             top: top,
@@ -24,6 +24,21 @@
         c.line1 = line1;
         c.line2 = line2;
 
+        return c;
+    }*/
+
+    function makeCircle(point) {
+        var c = new fabric.Circle({
+            left: point.x,
+            top: point.y,
+            strokeWidth: 5,
+            radius: 12,
+            fill: '#fff',
+            stroke: '#666'
+        });
+        c.hasControls = c.hasBorders = false;
+
+        c.point = point;
         return c;
     }
 
@@ -67,12 +82,48 @@
         return lines;
     }
 
-    var lines = generateLines(points);
-    console.log(lines);
+    /*function generateCircles(lines) {
+        console.log(lines);
+        var circles = [];
 
-    for (var i = 0; i < lines.length; i++) {
+        var circle = makeCircle(lines[0].get('x1'), lines[0].get('y1'), lines[points.length - 1], lines[0]);
+        circles.push(circle);
 
+        for (var i = 0; i < lines.length - 1; i++) {
+            circle = makeCircle(lines[i].get('x1'), lines[i].get('y1'), lines[i], lines[i+1]);
+            circles.push(circle);
+        }
+
+        return circles;
+    }*/
+
+    function generateCircles(points) {
+        var circles = [];
+
+        for (var i = 0; i < points.length; i++) {
+            var circle = makeCircle(points[i]);
+            circles.push(circle);
+        }
+
+        return circles;
     }
+
+    function drawElements(elements) {
+        for (var i = 0; i < elements.length; i++) {
+            canvas.add(elements[i]);
+        }
+    }
+
+    var polygon = makePolygon(points);
+    canvas.add(polygon);
+
+    var lines = generateLines(points);
+    var circles = generateCircles(points);
+
+    drawElements(lines);
+    drawElements(circles);
+
+    console.log(circles);
 
     // var line1 = makeLine([250, 125, 175, 225]),
     //     line2 = makeLine([175, 225, 200, 350]),
@@ -90,15 +141,18 @@
     //     makeCircle(line5.get('x1'), line5.get('y1'), line4, line5)
     // );
 
+
     canvas.on('object:moving', function (e) {
         var p = e.target;
-        p.line1 && p.line1.set({'x2': p.left, 'y2': p.top});
-        p.line2 && p.line2.set({'x1': p.left, 'y1': p.top});
+        //p.line1 && p.line1.set({'x2': p.left, 'y2': p.top});
+        //p.line2 && p.line2.set({'x1': p.left, 'y1': p.top});
+        p.point.x = p.left;
+        p.point.y = p.top;
         canvas.renderAll();
     });
 
-    var polygon = makePolygon(points);
-    canvas.add(polygon);
+
+    //canvas.add(makeCircle(points[0], polygon));
 
 })();
 
