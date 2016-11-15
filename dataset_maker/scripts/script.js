d986dc42b380e69f85a7438a38f18f64a93e51b3
Ugnelis <ugnius.malukas@gmail.com>
2016-11-16 00:01:53 +0200

    Added image upload.

--- dataset_maker/scripts/script.js
+++ dataset_maker/scripts/script.js
@@ -8,6 +8,7 @@
     var addButton = document.getElementById("add");
     var editButton = document.getElementById("edit");
     var myFieldset = document.getElementById("myFieldset");
+    var file = document.getElementById('file');
 
     function newPolygon() {
         if (addMode == false) {
@@ -53,10 +54,46 @@
         polygon.removePolygon();
     }
 
+    function handleFileSelect(object) {
+        var file = object.files[0];
+
+        // Only process image file.
+        if (!file.type.match('image.*')) {
+            return;
+        }
+
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (event) {
+                var image = new Image();
+                image.src = event.target.result;
+
+                // Set canvas size as the image size.
+                image.onload = function () {
+                    canvas.setDimensions({width: this.width, height: this.height});
+                    console.log(canvas.width);
+                };
+
+                canvas.setBackgroundImage(image.src, canvas.renderAll.bind(canvas), {
+                    originX: 'left',
+                    originY: 'top',
+                    left: 0,
+                    top: 0
+                });
+            };
+
+        })(file);
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(file);
+    }
+
     myFieldset.disabled = true;
     window.newPolygon = newPolygon;
     window.setData = setData;
     window.editData = editData;
     window.removeData = removeData;
+    window.handleFileSelect = handleFileSelect;
 
 })(window);
