2693208fa49795e7526a520205aa381acc4c7daa
Ugnelis <ugnius.malukas@gmail.com>
2016-12-26 17:11:53 +0200

    Added output value in read_files.

--- calculations/src/main/utils.py
+++ calculations/src/main/utils.py
@@ -4,8 +4,13 @@ import os.path
 import glob
 import json
 from PIL import Image
+import cv2
 
 resource = '../../../dataset'
+
+ROUTE_COLOR = (0, 153, 76)
+OBSTACLE_COLOR = (128, 0, 128)
+BOUNDARY_COLOR = (204, 0, 0)
 
 
 def read_files(dir):
@@ -33,12 +38,20 @@ def read_files(dir):
             print("JSON file '" + json_path + "' not found.")
             continue
 
+        # Read JSON data
         with open(json_path) as data_file:
             data = json.load(data_file)
 
+        # Create new image with regions
+        image_regions_path = dir + '/' + file_name + '_.jpg'
+        save_image_with_regions(file, data['polygons'])
+
+        # TODO make output as an array
         item = {
             'image': read_image(file),
-            'data': data
+            'output': read_image(image_regions_path),
+            # 'data': data,
+            # 'dir': file
         }
         result.append(item)
 
@@ -64,6 +77,33 @@ def save_image(image_tensor, image_name="image"):
     img = sess.run(image_tensor)
     img = Image.fromarray(img, "RGB")
     img.save(os.path.join(current_dir, image_name + ".jpg"))
+
+
+def save_image_with_regions(image_path, polygons):
+    image = cv2.imread(image_path)
+
+    image[:] = tuple(reversed(BOUNDARY_COLOR))  # Make whole image as a boundary
+    for polygon in polygons:
+        points_list = points_to_list(polygon['points'])
+        points_list = np.array(points_list, np.int32)  # points_list needs to be numpay array
+        if polygon['type'] == "route":
+            cv2.fillPoly(image, [points_list], ROUTE_COLOR)
+
+        if polygon['type'] == "obstacle":
+            cv2.fillPoly(image, [points_list], OBSTACLE_COLOR)
+
+    # Save image
+    file_name = os.path.splitext(image_path)
+    image_regions_path = file_name[0] + "_" + file_name[1]
+    cv2.imwrite(image_regions_path, image)
+
+
+def points_to_list(points):
+    points_list = []
+    for point in points:
+        points_list.append([int(point['x']), int(point['y'])])
+
+    return points_list
 
 
 def main():
