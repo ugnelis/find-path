c7fda5c114694133380a7a6072c4c4112c5c1620
Ugnelis <ugnius.malukas@gmail.com>
2017-02-22 22:29:58 +0200

    First step of training works!

--- calculations/main/test_fcn16_vgg.py
+++ calculations/main/test_fcn16_vgg.py
@@ -63,16 +63,28 @@ img1 = scp.misc.imread("./test_data/1.jpg")
 # output_image = tf.placeholder(tf.int32, shape=[None, 180, 320, 1])
 
 input_image = scp.misc.imread("./test_data/1.jpg")
-#output_image = scp.misc.imread("./test_data/1_.jpg", flatten=True)
+output_image = scp.misc.imread("./test_data/1_.jpg", flatten=True)
 
-
-input_image = input_image.reshape((1, 180, 320, 3))
-#output_image = output_image.reshape((1, 180, 320, 3)) # sita sutvarkyti ir turetu veikt
+#input_image = input_image.reshape((1, 180, 320, 3))
+# output_image = output_image.reshape((1, 180, 320, 3)) # sita sutvarkyti ir turetu veikt
 
 
 num_classes = 2
 
-output_image = np.ones((1, 180, 320, num_classes))
+output_grounds = np.zeros([180, 320, num_classes])
+
+# TODO fix this
+for i in range(180):
+    for j in range(320):
+        if output_image[i, j] == 255:
+            output_grounds[i, j, 0] = 1
+
+for i in range(180):
+    for j in range(320):
+        if output_image[i, j] == 0:
+            output_grounds[i, j, 1] = 1
+
+print(output_grounds.shape)
 
 with tf.device('/cpu:0'):
     with tf.Session() as sess:
@@ -93,7 +105,7 @@ with tf.device('/cpu:0'):
         with tf.name_scope("content_vgg"):
             vgg_fcn.build(batch_images, train=True, num_classes=num_classes)
 
-        cost = loss(vgg_fcn.upscore32, output_image, num_classes)
+        cost = loss(vgg_fcn.upscore32, true_out, num_classes)
         train = tf.train.AdamOptimizer(0.0001).minimize(cost)
 
         print('Finished building Network.')
@@ -109,16 +121,25 @@ with tf.device('/cpu:0'):
         tensors = [vgg_fcn.pred, vgg_fcn.pred_up]
         down, up = sess.run(tensors, feed_dict={images: img1})
 
+        # print(vgg_fcn.upscore32.eval())
+        # print (vgg_fcn.upscore32.eval())
+
         down_color = utils.color_image(down[0])
         up_color = utils.color_image(up[0])
 
         scp.misc.imsave('fcn16_downsampled.png', down_color)
         scp.misc.imsave('fcn16_upsampled.png', up_color)
 
+        if output_grounds is None:
+            print ("No values in personList")
+
+
         print('Training the Network')
         # simple 1-step training
-        down, up = sess.run(train, feed_dict={ images: img1, true_out: output_image})
+        sess.run(train, feed_dict={images: img1, true_out: output_grounds})
 
+        tensors = [vgg_fcn.pred, vgg_fcn.pred_up]
+        down, up = sess.run(tensors, feed_dict={images: img1})
         down_color = utils.color_image(down[0])
         up_color = utils.color_image(up[0])
 
