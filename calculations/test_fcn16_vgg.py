ac202c212cbe2e4829318320b0023e4ebf09b556
Ugnelis <ugnius.malukas@gmail.com>
2017-02-26 11:59:08 +0200

    Removed duplicated loss function.

--- calculations/test_fcn16_vgg.py
+++ calculations/test_fcn16_vgg.py
@@ -9,49 +9,13 @@ import scipy.misc
 import tensorflow as tf
 
 import fcn16_vgg
+import loss
 import utils
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.INFO,
                     stream=sys.stdout)
 
-
-def loss(logits, labels, num_classes, head=None):
-    """Calculate the loss from the logits and the labels.
-
-    Args:
-      logits: tensor, float - [batch_size, width, height, num_classes].
-          Use vgg_fcn.up as logits.
-      labels: Labels tensor, int32 - [batch_size, width, height, num_classes].
-          The ground truth of your data.
-      head: numpy array - [num_classes]
-          Weighting the loss of each class
-          Optional: Prioritize some classes
-
-    Returns:
-      loss: Loss tensor of type float.
-    """
-    with tf.name_scope('loss'):
-        logits = tf.reshape(logits, (-1, num_classes))
-        epsilon = tf.constant(value=1e-4)
-        logits = logits
-        labels = tf.to_float(tf.reshape(labels, (-1, num_classes)))
-
-        softmax = tf.nn.softmax(logits) + epsilon
-
-        if head is not None:
-            cross_entropy = -tf.reduce_sum(tf.mul(labels * tf.log(softmax),
-                                                  head), reduction_indices=[1])
-        else:
-            cross_entropy = -tf.reduce_sum(
-                labels * tf.log(softmax), reduction_indices=[1])
-
-        cross_entropy_mean = tf.reduce_mean(cross_entropy,
-                                            name='xentropy_mean')
-        tf.add_to_collection('losses', cross_entropy_mean)
-
-        loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
-    return loss
 
 
 img1 = scp.misc.imread("./test_data/1.jpg")
@@ -103,7 +67,7 @@ with tf.device('/cpu:0'):
         with tf.name_scope("content_vgg"):
             vgg_fcn.build(batch_images, train=True, num_classes=num_classes)
 
-        cost = loss(vgg_fcn.upscore32, true_out, num_classes)
+        cost = loss.loss(vgg_fcn.upscore32, true_out, num_classes)
         train = tf.train.AdamOptimizer(0.0001).minimize(cost)
 
         print('Finished building Network.')
